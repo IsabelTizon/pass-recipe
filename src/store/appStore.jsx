462274e0e35dfdc7c298849908d2a37118aa3ee1
IsabelTizon <tizonarranz@gmail.com>
2023-06-03 08:14:35 +0100

    delete course basket

--- src/store/appStore.jsx
+++ src/store/appStore.jsx
@@ -1,62 +1,72 @@
-import { create } from "zustand";
+// import { create } from "zustand";
+// import _ from "lodash";
 
-export const appStore = create((set) => ({
-	counterProducts: 0,
-	myItems: [],
-	// addProduct: (item) =>
-	// 	set((state) => ({
-	// 		counterProducts: state.counterProducts + 1,
-	// 		myItems: [...state.myItems, item],
-	// 	})),
-	addProduct: (item) => {
-		set((state) => {
-			return {
-				// spread operator to clone an array = [...state.myItems]
-				myItems: [...state.myItems, item],
-			};
-		});
-	},
+// export const appStore = create((set) => ({
+// 	counterProducts: 0,
+// 	myItems: [],
+// 	// addProduct: (item) =>
+// 	// 	set((state) => ({
+// 	// 		counterProducts: state.counterProducts + 1,
+// 	// 		myItems: [...state.myItems, item],
+// 	// 	})),
+// 	addProduct: (item) => {
+// 		set((state) => {
+// 			const myItems = [...state.myItems, item];
+// 			console.log("my items", myItems);
+// 			// --------------------------------------------------
 
-	increaseBtn: (item) => {
-		set((state) => {
-			console.log("state", state);
-			return {
-				// counterProducts: state.counterProducts + 1,
-				increase: () => set((state) => ({ quantity: state.quantity + 1 })),
-			};
-		});
-	},
+// 			const myItemsGroup = _.groupBy(myItems, (item) => item.id);
+// 			console.log("my Items Group", myItemsGroup);
+// 			//-----------------------------------------------------
+// 			//3 methods to convert objects into arrays
+// 			//..object.key() -  converts property names into arrays
+// 			//..object.values() -  converts property values into arrays
+// 			//..object.entries() -  converts both
+// 			Object.entries(myItemsGroup).forEach(([key, value]) =>
+// 				console.log("course", key, value)
+// 			);
+// 			console.log("========================");
+// 			return {
+// 				// spread operator to clone an array = [...state.myItems]
+// 				// myItems: [...state.myItems, item],
+// 				// const copyLoadash = [...copyMyItems];
+// 				// const grouped = _.groupBy(copyLoadash, (item) => item.id);
+// 				// console.log("loadash test", grouped);
+// 				// Object.entries(grouped).forEach(([key, value]) =>
+// 				// 	console.log("element", key, value)
+// 				// );
+// 			};
+// 		});
+// 	},
 
-	decreaseBtn: (item) => {
-		set((state) => {
-			console.log("state", state);
-			return {
-				// counterProducts: state.counterProducts + 1,
-				decrease: () => set((state) => ({ quantity: state.quantity - 1 })),
-			};
-		});
-	},
-	deleteBtn: (item) => {
-		set((state) => {
-			const index = state.myItems.findIndex((x) => x.id === item.id);
-			const copyMyItems = [...state.myItems];
-			copyMyItems.splice(index, 1);
+// 	increaseBtn: (item) => {
+// 		set((state) => {
+// 			console.log("state", state);
+// 			return {
+// 				// counterProducts: state.counterProducts + 1,
+// 				increase: () => set((state) => ({ quantity: state.quantity + 1 })),
+// 			};
+// 		});
+// 	},
 
-			// console.log(
-			// 	"index =",
-			// 	state.myItems.findIndex((x) => {
-			// 		console.log("=============================");
-			// 		console.log("esto es la x =>", x);
-			// 		console.log("esto es el id del elemento a eliminar =>", item.id);
-			// 		console.log("esto es el resultado del === =>", x.id === item.id);
-			// 		return x.id === item.id;
-			// 	})
-			// );
-
-			return { myItems: copyMyItems };
-		});
-	},
-}));
+// 	decreaseBtn: (item) => {
+// 		set((state) => {
+// 			console.log("state", state);
+// 			return {
+// 				// counterProducts: state.counterProducts + 1,
+// 				decrease: () => set((state) => ({ quantity: state.quantity - 1 })),
+// 			};
+// 		});
+// 	},
+// 	deleteBtn: (item) => {
+// 		set((state) => {
+// 			// const index = state.myItems.findIndex((x) => x.id === item.id);
+// 			// const copyMyItems = [...state.myItems];
+// 			// copyMyItems.splice(index, 1);
+// 			// return { myItems: copyMyItems };
+// 		});
+// 	},
+// }));
 
 // import { persist, createJSONStorage } from "zustand/middleware";
 
@@ -66,3 +76,25 @@ export const appStore = create((set) => ({
 // 		storage: createJSONStorage(() => sessionStorage),
 // 	})
 // );
+// ===========================================================================
+import { create } from "zustand";
+import _ from "lodash";
+
+export const appStore = create((set) => ({
+	counterProducts: 0,
+	myItems: [],
+	addProduct: (item) =>
+		set((state) => ({
+			counterProducts: state.counterProducts + 1,
+			myItems: [...state.myItems, item],
+		})),
+
+	deleteBtn: (item) => {
+		set((state) => {
+			const index = state.myItems.findIndex((x) => x.id === item.id);
+			const copyMyItems = [...state.myItems];
+			copyMyItems.splice(index, 1);
+			return { myItems: copyMyItems };
+		});
+	},
+}));
